fdfaaac0806f1ee828021906fc86d09fde492917
Cormac <cormacw13@gmail.com>
2022-01-27 12:09:16 -0500

    commit

--- train.py
+++ train.py
@@ -1,9 +1,13 @@
 from imageai.Detection.Custom import DetectionModelTrainer
+import json
+
+f = open('config.json')
+
+config = json.load(f)
 
 trainer = DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
-trainer.setDataDirectory(data_directory='./assets/dataset')
-trainer.setTrainConfig(object_names_array=["pool ball"], batch_size=4, num_experiments=200, train_from_pretrained_model="./models/pretrained-yolov3.h5")
-
+trainer.setDataDirectory(data_directory=config['data'])
+trainer.setTrainConfig(object_names_array=["pool ball"], batch_size=4, num_experiments=200, train_from_pretrained_model=config['pretrained_model'])
 
 trainer.trainModel()
