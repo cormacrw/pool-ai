8bf728232c618aa5c893286d7782aef8f4470841
Cormac <cormacw13@gmail.com>
2022-03-17 18:17:04 -0400

    fixed trained

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ data_dir = sys.argv[1]
 
 trainer = DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
-trainer.setDataDirectory(data_directory=config['data'])
+trainer.setDataDirectory(data_directory=data_dir)
 trainer.setTrainConfig(object_names_array=["pool ball"], batch_size=4, num_experiments=200, train_from_pretrained_model=pretrain_model)
 
 trainer.trainModel()
